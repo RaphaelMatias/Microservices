a7b21fc31f8ccb9b6e201a514e798d4f72c45ce3
Raphael Matias <phael110199@gmail.com>
2023-11-17 15:43:33 -0300

    Creation of Product class and some configurations

---
+++ first_microservice/src/main/java/br/com/castro/first_microservice/model/Product.java
@@ -0,0 +1,61 @@
+package br.com.castro.first_microservice.model;
+
+public class Product {
+
+    //#region attributes
+    private Integer Id;
+
+    private String name;
+
+    private Integer quantity;
+
+    private Double price;
+
+    private String observation;
+    //#endregion
+
+
+    //#region Getters and Setters
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+    //#endregion
+
+
+}
