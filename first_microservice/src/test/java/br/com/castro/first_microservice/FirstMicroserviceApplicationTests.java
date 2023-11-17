127541692007afba7a4ab1eabba14acab76b6965
Raphael Matias <phael110199@gmail.com>
2023-11-17 14:49:36 -0300

    Creation of spring boot project

---
+++ first_microservice/src/test/java/br/com/castro/first_microservice/FirstMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.castro.first_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
